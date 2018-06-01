a349c2740a13cbb5bd8335a1ea001b7500d4793a
Vette <stephan.vette@tu-dortmund.de>
2018-06-01 10:00:39 +0100

    Init proc

---
+++ Initialization/ConsoleApp3/ConsoleApp3/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Position
+    {
+        public int X=0;
+        public int Y=0;
+
+    }
+    public class PositionD
+    {
+        public double X = 0;
+        public double Y = 0;
+
+    }
+}
